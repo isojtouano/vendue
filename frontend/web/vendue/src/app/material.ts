d93f4be467ae564aa87ae0fb8eccaed93b726902
isojtouano <renzo.quano@takingyouforward.org>
2019-02-13 16:47:06 +0800

    Some progress

--- frontend/web/vendue/src/app/material.ts
+++ frontend/web/vendue/src/app/material.ts
@@ -1,9 +1,24 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatCheckboxModule, MatToolbarModule } from '@angular/material';
+import { MatCardModule, MatButtonModule, MatCheckboxModule, MatToolbarModule, MatInputModule, MatMenuModule } from '@angular/material';
+import { NgMatSearchBarModule } from 'ng-mat-search-bar';
 
 @NgModule({
-    imports: [ MatButtonModule, MatCheckboxModule, MatToolbarModule ],
-    exports: [ MatButtonModule, MatCheckboxModule, MatToolbarModule ],
+    imports: [ MatButtonModule, 
+        MatCheckboxModule, 
+        MatToolbarModule, 
+        MatInputModule, 
+        NgMatSearchBarModule, 
+        MatMenuModule,
+        MatCardModule
+    ],
+    exports: [ MatButtonModule, 
+        MatCheckboxModule, 
+        MatToolbarModule, 
+        MatInputModule, 
+        NgMatSearchBarModule, 
+        MatMenuModule,
+        MatCardModule
+    ],
 })
 
 export class MaterialModule {}
